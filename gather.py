3895e2a7f01a58ed620ca7cffd93447cd4db997b
Joseph Roybal <39634437+JoeRoybal@users.noreply.github.com>
2021-12-12 19:41:40 -0600

    did work dawg
    There is an issue where pokemon with special characters cause issues. fix PokeDex.json to fix

--- gather.py
+++ gather.py
@@ -1,21 +1,110 @@
 import requests
+import random
 from bs4 import BeautifulSoup
 import json
 
-URL = "https://pokemondb.net/pokedex/all"
-page = requests.get(URL)
-key = 445
-
-soup = BeautifulSoup(page.content, "html.parser")
 with open('PokeDex.json') as f:
     PokeDex = json.load(f)
+nationalNo = random.randint(1, 898)
+key_list = list(PokeDex.keys())
+value_list = list(PokeDex.values())
+for i in range(0, len(key_list)):
+    key_list[i] = int(key_list[i])
 
-for tr in soup.find_all('tr')[1:]:
-    tds = tr.find_all('td')
+pokedexnum = key_list.index(nationalNo)
+pokemon = value_list[pokedexnum]
+URL = "https://pokemondb.net/pokedex/"+pokemon.split()[0]
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
 
-# TODO:
-# -decide random dex num
-# -find pokemon from pokedex json
-# -scape db for info
-# -format info for page
-# -set up refresh /timer for new pokemon?
+main = soup.find(id="main")
+name = main.find("h1").text
+pokedexData = main.find(class_="vitals-table").text
+baseStats = main.find(class_="grid-col span-md-12 span-lg-8").text
+# needs reformating, not sure if it will be included yet...
+typeDefenses = main.find(class_="resp-scroll text-center").text
+img = "<a href=\"http://pokemondb.net/pokedex/"+pokemon.lower() + \
+    "\"><img src=\"https://img.pokemondb.net/sprites/home/normal/" + \
+    pokemon.lower()+".png\" alt="+pokemon+"></a>"
+shinyImg = "<a href=\"http://pokemondb.net/pokedex/"+pokemon.lower()+"\"><img src=\"https://img.pokemondb.net/sprites/home/shiny/" + \
+    pokemon.lower()+".png\" alt=\""+pokemon+"\"></a>"
+
+
+typeBegin = pokedexData.find('Type')
+typeEnd = pokedexData.find('Species')
+typeTypes = pokedexData[typeBegin+6:typeEnd-3]
+
+speciesBegin = pokedexData.find('Species')
+speciesEnd = pokedexData.find('Height')
+species = pokedexData[speciesBegin+8:speciesEnd-3]
+heightBegin = pokedexData.find('Height')
+heightEnd = pokedexData.find('Weight')
+height = pokedexData[heightBegin+7:heightEnd-3]
+weightBegin = pokedexData.find('Weight')
+weightEnd = pokedexData.find('Abilities')
+weight = pokedexData[weightBegin+7:weightEnd-3]
+abilitiesBegin = pokedexData.find('Abilities')
+abilitiesEnd = pokedexData.find('Local')
+abilities = pokedexData[abilitiesBegin+13:abilitiesEnd-3]
+firstCheck = '2. '
+secondCheck = 'hidden ability'
+ability1 = ''
+ability2 = ''
+ability3 = ''
+
+
+def getHiddenAbility(String):
+    sus = String.split()
+    count = 0
+    keyPhrase = 'NONE'
+    for x in sus:
+        for index in range(len(x)):
+            if x[index].isupper():
+                count += 1
+        if count > 1:
+            keyPhrase = x
+        count = 0
+    for index in range(len(keyPhrase)):
+        if keyPhrase[index].isupper():
+            Last = index
+    hidden = keyPhrase[Last:]
+    hiddenAbility = String.find(hidden)
+    return hiddenAbility
+
+
+if firstCheck in abilities and secondCheck in abilities:
+    ability1 = abilities[:abilities.find('2.')]
+    hiddenAbility = getHiddenAbility(abilities)
+    ability2 = abilities[abilities.find('2.')+3:hiddenAbility]
+    ability3 = abilities[hiddenAbility:]
+elif firstCheck in abilities and not secondCheck in abilities:
+    ability1 = abilities[:abilities.find('2.')]
+    ability2 = abilities[abilities.find('2.')+3:]
+elif firstCheck not in abilities and secondCheck in abilities:
+    hiddenAbility = getHiddenAbility(abilities)
+    ability1 = abilities[:hiddenAbility]
+    ability2 = abilities[hiddenAbility:]
+else:
+    pass
+localBegin = pokedexData.find('Local')
+local = pokedexData[localBegin+8:-3]
+
+HPBegin = baseStats.find('HP')
+HPEnd = baseStats.find('Attack')
+HP = baseStats[HPBegin+3:HPEnd-3].split()
+attackBegin = baseStats.find('Attack')
+attackEnd = baseStats.find('Defense')
+attack = baseStats[attackBegin+7:attackEnd-3].split()
+defenseBegin = baseStats.find('Defense')
+defenseEnd = baseStats.find('Sp.')
+defense = baseStats[defenseBegin+8:defenseEnd].split()
+spAtkBegin = baseStats.find('Sp. Atk')
+spAtkEnd = baseStats.find('Sp. Def')
+spAtk = baseStats[spAtkBegin+8:spAtkEnd].split()
+spDefBegin = baseStats.find('Sp. Def')
+spDefEnd = baseStats.find('Speed')
+spDef = baseStats[spDefBegin+8:spDefEnd].split()
+speedBegin = baseStats.find('Speed')
+speedEnd = baseStats.find('Total')
+speed = baseStats[speedBegin+6:speedEnd].split()
+total = baseStats[speedEnd+5:speedEnd+10].split()
--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 from flask import Flask, render_template
+import gather
 
 app = Flask(__name__)
 
 
 @app.route('/')     # main URL page
 def index():
-    return render_template('index.html')
+    nationalNo = gather.nationalNo
+    pokemon = gather.pokemon
+    img = gather.img
+    shinyImg = gather.shinyImg
+    typeTypes = gather.typeTypes
+    species = gather.species
+    height = gather.height
+    weight = gather.weight
+    ability1 = gather.ability1
+    ability2 = gather.ability2
+    ability3 = gather.ability3
+    HP = gather.HP
+    attack = gather.attack
+    defense = gather.defense
+    spAtk = gather.spAtk
+    spDef = gather.spDef
+    speed = gather.speed
+    total = gather.total
+
+    return render_template('index.html',
+                           nationalNo=nationalNo,
+                           pokemon=pokemon,
+                           img=img,
+                           shinyImg=shinyImg,
+                           typeTypes=typeTypes,
+                           species=species,
+                           height=height,
+                           weight=weight,
+                           ability1=ability1,
+                           ability2=ability2,
+                           ability3=ability3,
+                           HP=HP,
+                           attack=attack,
+                           defense=defense,
+                           spAtk=spAtk,
+                           spDef=spDef,
+                           speed=speed,
+                           total=total)
 
 
 if __name__ == '__main__':
